c4fda0b5ad998b80f4f03e83c9caeccbd3c82227
James Beavers <jamesjbeavers@gmail.com>
2022-09-05 17:13:58 -0400

    stash

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 Super naive install script, links all files/directories to ~/.fileName
 excluding README.md and install.sh
 
+pre-req: install nvm/node/yarn
+
 * yarn
 * ./install.sh
 * open vim, :PlugInstall
--- install.sh
+++ install.sh
@@ -23,6 +23,7 @@ for file in $(ls -p ./scripts/*); do
   basefile="$(basename $absfile)"
   dirfile="$(dirname $absfile)"
   echo "Linking $HOME/.local/bin/$basefile --> $absfile"
+  mkdir -p "$HOME/.local/bin"
   ln -nfs $absfile "$HOME/.local/bin/$basefile"
 done
 
@@ -30,64 +31,62 @@ done
 #ln -nfs "$(realpath ./other/kitty/kitty.conf)" "$HOME/.config/kitty/kitty.conf"
 
 ## neovim - use shared vimrc and runtime paths
-#echo "Setting up init.vim to use shared vim config"
-#mkdir -p ~/.config/nvim
-#cat <<EOF > ~/.config/nvim/init.vim
-#set runtimepath^=~/.vim runtimepath+=~/.vim/after
-#let &packpath=&runtimepath
-#source ~/.vimrc
-#EOF
+echo "Setting up init.vim to use shared vim config"
+mkdir -p ~/.config/nvim
+cat <<EOF > ~/.config/nvim/init.vim
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath=&runtimepath
+source ~/.vimrc
+EOF
 
-#first_time_setup () {
-  ##brew bundle
-
+first_time_setup () {
   ## Nvm, node lts
-  ##curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-  ##source ~/.bash_profile
-  ##nvm install lts/*
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+  source ~/.profile
+  nvm install lts/*
 
   ## vim-plug, for vim plugin loading later
-  #curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    #https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-#}
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+}
 
-#if ! command -v ngrok 2> /dev/null; then
-  #first_time_setup
-#fi
+if ! command -v node 2> /dev/null; then
+  first_time_setup
+fi
 
 #if [ "$1" = "fast" ]; then
   #echo "'fast' specified. Exiting."
   #exit 0;
 #fi
 
-#echo "Installing nvim CoC extensions"
+echo "Installing nvim CoC extensions"
 
 ## Note: extensions are auto updated. However, auto update isn't support for
 ## in-line Plug based install via vimrc, so have to install this way :(
 ## Also, this hangs at the end :(
-##nvim --headless -c "$(cat <<EOF
 #nvim -c "$(cat <<EOF
-#:CocInstall \
-#coc-tsserver \
-#coc-json \
-#coc-html \
-#coc-yaml \
-#coc-css \
-#coc-python \
-#coc-jest \
-#coc-tslint-plugin \
-#coc-eslint \
-#coc-docker
-#EOF
-#)"
+nvim --headless -c "$(cat <<EOF
+:CocInstall \
+coc-tsserver \
+coc-json \
+coc-html \
+coc-yaml \
+coc-css \
+coc-pyright \
+coc-jest \
+coc-tslint-plugin \
+coc-eslint \
+coc-docker
+EOF
+)"
 
-##NVIM_PID=$?
+NVIM_PID=$?
 
-##trap "echo \"Killed NVM PID $NVIM_PID\"" TERM
+trap "echo \"Killed NVM PID $NVIM_PID\"" TERM
 
-##echo "Giving Neovim :CocInstall 20 seconds to complete..."
-##sleep 20
-##echo ""
-##echo "Killing install because it hangs indefinitely. Hopefully it finished..."
-##kill $NVIM_PID
-#echo "Install complete."
+echo "Giving Neovim :CocInstall 20 seconds to complete..."
+sleep 20
+echo ""
+echo "Killing install because it hangs indefinitely. Hopefully it finished..."
+kill $NVIM_PID
+echo "Install complete."
